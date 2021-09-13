b1ec828974b2bb9ac885b25a4ede19426c27ac5f
shree7890 <shreekantadash831@gmail.com>
2021-09-13 16:38:44 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -6,8 +6,7 @@ const loadProducts = () => {
     .then((data) => showProducts(data));
 };
 loadProducts();
-
-// show all product in UI 
+// show all product in web UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
@@ -19,10 +18,14 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h4 class = "mt-4 fs-4 primary fst-italic">${product.title.slice(0, 15)}</h4>
-      <h6><span class ="text">${product.rating.rate} Ratings</span> | <span class = "text-success">Counts: ${product.rating.count}</span> </h6>
+      <span class="fs-6 text ">Ratings: </span>
+      <span class="text-warning fs-6 fw-bold">${iconShow(product.rating.rate)} <span class="text-secondary ">(${product.rating.rate})</span></span>
+      <h6 class= " mt-2">
+        <span class = "text-success">Counts: ${product.rating.count}</span> 
+     </h6>
       <p class= "danger fst-italic">Category: ${product.category}</p>
-      <h3><span class = "price">Price:</span> <span class = "span">$ ${product.price}</span></h3>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
+      <h3 class="mb-3"><span class = "price">Price:</span> <span class = "span">$ ${product.price}</span></h3>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">Add to cart</button>
       <button id="details-btn" onclick="getDetail('${product.id}')" class="btn clickbtn">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
@@ -33,15 +36,14 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
-  document.getElementById("total-Products").innerText = count;
   updateTotal();
+  document.getElementById("total-Products").innerText = count;
 };
 // get input data
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -83,14 +85,14 @@ const updateTotal = () => {
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
-// details product 
+// details single product id api
 const getDetail = (id) => {
   const url = `https://fakestoreapi.com/products/${id}`;
   fetch(url)
     .then(res => res.json())
     .then(data => productDetail(data));
 }
-// single detail product show
+// detail single product show
 const productDetail = (detail) => {
   const singleProduct = document.getElementById("details-products");
   singleProduct.innerHTML = `
@@ -105,4 +107,22 @@ const productDetail = (detail) => {
   </div>
   `;
 }
+// rating star show for client feedback
+const iconShow = (rate) => {
+  const rateIcon = Math.floor(rate);
+  let iconStore = "";
+
+  for (let i = 0; i < rateIcon; i++) {
+    iconStore += `<i class="bi bi-star-fill"></i>`;
+  }
+  iconStore += `<i class="bi bi-star-half"></i>`;
+
+  if (5 - rateIcon > 1) {
+    for (let i = 0; i < 5 - rateIcon - 1; i++) {
+      iconStore += `<i class="bi bi-star"></i>`;
+    }
+  }
+
+  return iconStore;
+};
 
